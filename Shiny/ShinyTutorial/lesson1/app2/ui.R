57aa1a048e14fc92e469cd29ec9d27490b8ea807
BabitzdaBrave <amg.vintila@gmail.com>
2015-07-29 01:48:26 -0700

    why does it say there is no library(ISwR)? and why is there problem loading helvetica arial font when doing a simple hist(y) from Crawley book?

--- Shiny/ShinyTutorial/lesson1/app2/ui.R
+++ Shiny/ShinyTutorial/lesson1/app2/ui.R
@@ -1,8 +1,29 @@
+# tutorial link: shiny.rstudio.com/tutorial/lesson2/
+
 shinyUI(fluidPage(
-  titlePanel("title panel"),
+  titlePanel("My Shiny App"),
   
-  sidebarLayout(position="right",
+  sidebarLayout(#position="right",
     sidebarPanel("sidebar panel"),
-    mainPanel("main panel")
+    mainPanel(
+      h1("First level title"),
+      h2("Second  level title"),
+      h3("Third  level title"), 
+      h4("Fourth level title"), 
+      h5("Fifth level title"),
+      h6("Sixth  level title"),
+      
+      p("p creates a paragraph of text."),
+      p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'Monaco'; font-si16pt"),
+      strong("strong() makes bold text."),
+      em("em() creates italicized (i.e, emphasized) text."),
+      br(),
+      code("code displays your text similar to computer code"),
+      div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+      br(),
+      p("span does the same thing as div, but it works with",
+        span("groups of words", style = "color:blue"),
+        "that appear inside a paragraph.")
+    )
   )
 ))
---
+++ StatisticsIntroUsingR_Crawley/Chapter3: CentralTendency.R
@@ -0,0 +1,7 @@
+
+setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/StatisticsIntroUsingR_Crawley")
+yvals <- read.csv("data/yvalues.csv")
+attach(yvals)
+
+hist(y)
+y
