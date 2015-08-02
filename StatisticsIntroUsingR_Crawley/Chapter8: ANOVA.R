83bdb8934da52afa4a18ca3e0bda7e202a923b70
BabitzdaBrave <amg.vintila@gmail.com>
2015-08-02 11:10:29 -0700

    on chapter 8 and understood ANOVA for means F test

--- StatisticsIntroUsingR_Crawley/Chapter8: ANOVA.R
+++ StatisticsIntroUsingR_Crawley/Chapter8: ANOVA.R
@@ -7,5 +7,76 @@
 in a multi-way ANOVA to test if the response to one factor depends 
 on the level of another factor. "
 
+setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/StatisticsIntroUsingR_Crawley")
 
 # (1) ONE-WAY ANOVA
+oneway <- read.csv("data/oneway.csv")
+attach(oneway)
+oneway
+
+plot(1:20, ozone, ylim=c(0,8), ylab="y", xlab="order", pch=19, col="red")
+abline(h=mean(ozone), col="blue")
+# segments
+for(i in 1:20)
+  lines(c(i, i), c(mean(ozone), ozone[i]), col="green")
+
+# look at departure of data from means of Garden A and B
+plot(ozone, ylim=c(0, 8), ylab="y", xlab="order", pch=19, bg=as.numeric(garden))
+abline(h=mean(ozone[garden=="A"]))
+abline(h=mean(ozone[garden=="B"]), col="red")
+
+# segments
+# this code with index[i] works since data was plotted in order
+index <- 1:length(ozone)
+for(i in 1:length(index)) {
+  if(garden[i] == "A")
+    lines(c(index[i], index[i]), c(mean(ozone[garden=="A"]), ozone[i]))
+  else
+    lines(c(index[i], index[i]), c(mean(ozone[garden=="B"]), ozone[i]), col="red")
+}
+
+
+# ANOVA analysis: is this difference in mean ozone in A and B significantly big? Or could it have occurred by chance alone?
+# IMPORTANT: if means are significantly different, sum of squares from individual treatment means is smaller than sum of squares from overall mean
+
+# SSE = variation within the sample (made of many SSEs for each sample)
+# SSA = variation between each sample mean
+# SSY = SSA + SSE
+SSY <- sum((ozone - mean(ozone))^2); SSY
+SSE <- 
+  sum((ozone[garden=="A"] - mean(ozone[garden=="A"]))^2) +
+  sum((ozone[garden=="B"] - mean(ozone[garden=="B"]))^2); SSE
+SSA <- SSY - SSE; SSA
+
+# build ANOVA table
+varY <- SSY/19; varY # total
+varE <- SSE/18; varE # error
+varA <- SSA/1; varA # garden
+
+"
+ H0: mu1 = mu2 = mu3 .... 
+ H1: at least one mean from the sample groups is different"
+# If the variation between sample means is significantly greater than variation within each sample, then the  means must be different. 
+f.ratio <- varA/varE; f.ratio
+1 - pf(f.ratio, df1=1, df2=18) # therefore, the two means are significantly different
+
+# Do the easy way
+summary(aov(ozone~garden))
+# df residuals = k(n-1) = (2 garden levels) * (10 replicates per garden - 1) = 2(10-1) = 2* 9 = 18
+
+
+# Do graphical check of assumptions of model (constant variance and normal errors)
+plot(aov(ozone~garden))
+# first plot - shows that variances are identical in the two treatments
+# second plot: values within each treatment group are normal
+# third plot: residuals show constant variance
+# fourth plot: attention to the values with large residuals
+
+
+
+
+# Finding SSA directly (shortcut)
+cbind(ozone[garden=="A"], ozone[garden=="B"])
+tapply(ozone, garden, sum)
+T1 <- 30; T2 <- 50
+# SSA = sum(Ti^2)/n - (sum(y))^2/(kn)
