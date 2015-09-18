6f02b2d9328c9c8320111ec77098eb94426422ba
BabitzdaBrave <amg.vintila@gmail.com>
2015-09-18 07:11:48 -0700

    almost done exercises from chapter 2 of FPP hyndman book

--- RStats/learneconometrics/FPP/ch2: forecaster's toolbox.R
+++ RStats/learneconometrics/FPP/ch2: forecaster's toolbox.R
@@ -1,10 +1,12 @@
 #install.packages("fpp")
-install.packages("GGally")
-
+#install.packages("GGally")
+#install.packages("reshape")
 library(fpp)
 library(ggplot2)
 library(ggfortify)
 library(GGally)
+library(reshape)
+library(plyr)
 
 melsyd
 autoplot(melsyd[, "Economy.Class"], main="Economy class 
@@ -105,5 +107,184 @@ beerfit1 = meanf(beer2, h=11)
 beerfit2 = naive(beer2, h=11)
 beerfit3 = snaive(beer2, h=11)
 
-autoplot(beerfit1, main="Forecasts for quarterly beer production")
+autoplot(beer2)
+plot(beer2, main="Forecasts for quarterly beer production")
+# @todo: how to add these data to autoplot?
+lines(beerfit1$mean, col=4)
 lines(beerfit2$mean, col=2)
+lines(beerfit3$mean, col=3)
+legend("topright", lty=1, col=c(4,2,3), 
+       legend=c("Mean method", "Naive method", "Seasonal naive method"))
+
+
+'# plot with ggplot2
+beer.mat = matrix(as.vector(beer2), ncol=4, byrow=TRUE)
+dates = matrix(rep(1992:2005, 4), ncol=4, byrow=FALSE)
+
+beer.df = data.frame(character(), character())
+for(i in 1:nrow(dates)){
+  mat = cbind(dates[i,], beer.mat[i,])
+  beer.df = rbind(beer.df, as.data.frame(mat))
+}
+beer.df = rename(beer.df, c("V1"="Date", "V2"="Quarter"))
+beer.df
+
+ggplot(beer.df, aes(Date, Quarter)) + geom_line()
+'
+
+# Forecasting applied to Dow Jones index
+dj2 = window(dj, end=250)
+plot(dj2, main="Dow Jones Index (daily ending 15 July 1994)", 
+         ylab="", xlab="Day", xlim=c(2, 290))
+lines(meanf(dj2, h=42)$mean, col=4)
+lines(naive(dj2, h=42)$mean, col=2)
+lines(rwf(dj2, drift=TRUE, h=42)$mean, col=3)
+legend("topleft", lty=1, col=c(4,2,3),
+       legend=c("Mean method", "Naive method", "Drift method"))
+lines(dj)
+
+
+
+
+# 2.4 Transformations
+
+# Box cox transformations depend on lambda parameter: 
+# wt = ln(yt) if lambda=0,  and (yt^lambda-1)/lambda otherwise
+# lambda makes seasonality across series the same
+
+autoplot(log(elec), ylab="Transformed electricity demand", 
+         xlab="Year", main="Transformed monthly electricity demand")
+title(main="log", line=-1) # log(lambda=0)
+
+yt = elec
+wt = (elec^lambda-1)/lambda
+lambda = BoxCox.lambda(yt); lambda
+autoplot(BoxCox(yt, lambda))
+autoplot(wt) #the same
+
+# Backtransforming: 
+# yt = exp(wt) if lambda=0 and (lambda*wt + 1)^(1/lambda) otherwise
+autoplot((lambda*wt + 1)^(1/lambda))
+autoplot(yt) #the same as above
+
+
+# Calendar Variation
+monthdays = rep(c(31,28,31,30,31,30,31,31,30,31,30,31),14)
+monthdays[26 + (4*12)*(0:2)] = 29 # leap year
+monthdays
+autoplot(milk, main="Monthly milk production per cow", ylab="Pounds", 
+         xlab="Years")
+# Now remove variation due to differing month lengths
+autoplot(milk/monthdays, main="Average milk production
+         per cow per day", ylab="Pounds", xlab="Years")
+
+
+
+
+# 2.5 Evaluating Forecast Accuracy
+
+# ei = forecast error = yi - yi_forecast
+# MAE (mean absolute error) = mean(|ei|), 
+# RMSE (root mean squared error) = sqrt(mean(ei^2))
+# MAPE (mean absolute % error) = mean(|100ei/yi|)
+
+plot(beerfit1, plot.conf=FALSE, main="Forecasts for quarterly
+     beer production")
+lines(beerfit2$mean, col=2)
+lines(beerfit3$mean, col=3)
+lines(ausbeer)
+legend("topright", lty=1, col=c(4,2,3), 
+       legend=c("Mean method", "Naive method", "Seasonal naive method"))
+
+
+beer3 = window(ausbeer, start=2006)
+accuracy(beerfit1, beer3)
+accuracy(beerfit2, beer3)
+accuracy(beerfit3, beer3)
+
+
+dj3 = window(dj, start=251)
+accuracy(meanf(dj2, h=42), dj3)
+accuracy(rwf(dj2, h=42), dj3)
+accuracy(rwf(dj2, drift=TRUE, h=42), dj3)
+
+
+
+
+# 2.6 Residual Diagnostics
+
+dj2 = window(dj, end=250)
+autoplot(dj2, main="Dow Jones Index (daily ending 15 Jul 94)", xlab="Day")
+res = residuals(naive(dj2), na.rm=T)
+res = na.omit(res)
+autoplot(res, main="Residuals from naive method", xlab="Day")
+autoplot(acf(res, main="ACF of residuals from naive method", plot=FALSE))
+
+# Portmanteau test: 
+# Use lag=10 for non-seasonal data and lag=2m for seasonal data, 
+# where m is period of seasonality
+# Use lag = T/5 if those values are larger than T/5
+# Smaller statistic = lower p-value = more normal residuals
+
+Box.test(res, lag=10, fitdf=0)
+Box.test(res, lag=10, fitdf=0, type="Lj")
+
+
+
+
+
+# ---------------- EXERCISES ------------------
+library(fma)
+
+# 1a
+autoplot(dole, main="Monthly total people on unemployed benefits
+         in Australia (Jan 1956 - Jul 1992", xlab="Year")
+# 1b
+autoplot(usdeaths, main="Number of accidental US deaths", xlab="Year")
+# transform: 
+autoplot(diff(usdeaths))
+autoplot(log(usdeaths))
+# 1c
+autoplot(bricksq, 
+         main="Quarterly brick production in Australia", 
+         xlab="Years")
+lambda = BoxCox.lambda(bricksq); lambda
+autoplot(BoxCox(bricksq, lambda))
+# 2a --- 
+autoplot(dowjones, main="Dow Jones Data")
+# 2b
+driftMethod = rwf(dowjones, drift=TRUE, h=10)
+plot(dowjones)
+lines(driftMethod$mean, col=3)
+
+firstObs = dowjones[1]
+lastObs = dowjones[length(dowjones)]
+
+mean(c(firstObs, lastObs))
+driftMethod$mean
+# none of the means are equal! How to answer question 2c?
+
+# 2d
+dowjones
+dowjones2 = window(dowjones, start=1, end=67)
+djfit1 = meanf(dowjones2, h=10)
+djfit2 = naive(dowjones2, h=10)
+djfit3 = snaive(dowjones2, h=10)
+djfit4 = rwf(dowjones2, drift=TRUE, h=10)
+
+dowjones3 = window(dowjones, start=68)
+accuracy(djfit1, dowjones3)[2, c(2,3,5,6)]
+accuracy(djfit2, dowjones3)[2, c(2,3,5,6)]
+accuracy(djfit3, dowjones3)[2, c(2,3,5,6)]
+accuracy(djfit4, dowjones3)[2, c(2,3,5,6)]
+
+# therefore, naive method is better
+
+# 3a ---
+autoplot(ibmclose, main="IBM stock daily closing prices", xlab="Day")
+# 3b
+trainingSet = sample(ibmclose, 
+                     size=round(0.20*length(ibmclose), 0), 
+                     replace=FALSE)
+testSet = 
+ibmclose
