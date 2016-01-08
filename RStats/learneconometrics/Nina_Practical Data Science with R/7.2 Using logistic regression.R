cb651db5a4ce31df74a2d7cd78b7172c72731f59
BabitzdaBrave <amg.vintila@gmail.com>
2016-01-08 05:19:16 -0800

    did code about threshold and recall...?

--- RStats/learneconometrics/Nina_Practical Data Science with R/7.2 Using logistic regression.R
+++ RStats/learneconometrics/Nina_Practical Data Science with R/7.2 Using logistic regression.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/RStats/learneconometrics/Nina_Practical Data Science with R")
 
 load("data/NatalRiskData.rData")
@@ -24,4 +26,53 @@ train$pred <- predict(model, newdata=train, type="response")
 test$pred <- predict(model, newdata=test, type="response")
 
 
+# Predictions grouped by known outcome
+ggplot(data=train, aes(x=pred, color=atRisk, linetype=atRisk)) + 
+      geom_density(size=1)
 
+
+
+# Model trade-offs
+library(ROCR)
+#install.packages("ROCR")
+library(grid)
+
+predObj <- prediction(train$pred, train$atRisk)
+precObj <- performance(predObj, measure="prec") #precision as function of threshold
+recObj <- performance(predObj, measure="rec") # recall as function of threshold
+
+precision <- (precObj@y.values)[[1]]
+prec.x <- (precObj@x.values)[[1]]
+recall <- (recObj@y.values)[[1]]
+# xvalues (thresholds) are same in predObj and recObj
+
+rocFrame <- data.frame(threshold=prec.x, precision=precision, recall=recall)
+head(rocFrame)
+
+#Function to plot many stacked plots on one page
+nplot <- function(plist) {
+      n <- length(plist)
+      grid.newpage()
+      pushViewport(viewport(layout=grid.layout(n, 1)))
+      vplayout = function(x, y) {
+            viewport(layout.pos.row=x, layout.pos.col=y)
+      }
+      for (i in 1:n){
+            print(plist[[i]], vp=vplayout(i, 1))
+      }
+}
+
+# find rate of at-risk births in training set
+pnull <- mean(as.numeric(train$atRisk))
+
+#Plot enrichment rate as function of threshold
+p1 <- ggplot(rocFrame, aes(x=threshold)) + 
+      geom_line(aes(y=precision/pnull)) + 
+      coord_cartesian(xlim=c(0, 0.05), ylim=c(0, 10))
+
+# Plot recall as function of threshold
+p2 <- ggplot(rocFrame, aes(x=threshold)) + 
+      geom_line(aes(y=recall)) + 
+      coord_cartesian(xlim=c(0, 0.05))
+
+nplot(list(p1, p2))
