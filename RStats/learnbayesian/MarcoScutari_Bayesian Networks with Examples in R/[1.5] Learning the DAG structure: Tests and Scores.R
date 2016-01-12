4bc843faf3c27fb05a63c39fe462588c9f0081c8
BabitzdaBrave <amg.vintila@gmail.com>
2016-01-12 03:14:27 -0800

    on page 19 comparing rnd dag with our dag in terms of BIC score

--- RStats/learnbayesian/MarcoScutari_Bayesian Networks with Examples in R/[1.5] Learning the DAG structure: Tests and Scores.R
+++ RStats/learnbayesian/MarcoScutari_Bayesian Networks with Examples in R/[1.5] Learning the DAG structure: Tests and Scores.R
@@ -8,16 +8,40 @@ survey <- read.table("data/survey.txt", header=TRUE)
 head(survey)
 
 
-### Conditional Independence Tests: identify presence of individual arcs
+### Conditional Independence Tests: identify presence of individual arcs (individual)
+
 # if H0 for conditional indep is rejected, arc is included in DAG
 df <- (nlevels(survey[, "T"]) - 1) * (nlevels(survey[, "E"]) - 1) * 
       nlevels(survey[, "O"]) * nlevels(survey[, "R"]); df
 
 # Test if conditionally independent
 # H0 = T is independent of E | (O & R)
-ci.test("T", "E", c("O", "R"), test="mi", data=survey) # G^2 test
+ci.test("T", "E", c("O", "R"), test="mi", data=survey) # G^2 tests
 ci.test("T", "E", c("O", "R"), test="x2", data=survey) # chi-square test
 
-# Should we remove O -> T?
-# H0 = T is independent of O|R
+# Should we remove O -> T? What about R-> T?
+# H0 = T is independent of O|R 
 ci.test("T", "O", "R", test = "x2", data=survey)
+
+# Test all - strength = p-value
+arc.strength(dag, data = survey, criterion = "x2")
+
+
+
+
+### Network Scores: how well DAG mirrors dependence structure of data (as whole)
+# Types of scores: BIC, BDeu
+
+score(dag, data = survey, type="bic")
+score(dag, data = survey, type="bde", iss=10)
+
+# does dag fit data better BEFORE or AFTER adding arc E -> T?
+dag4 <- set.arc(dag, from="E", to="T")
+nparams(dag4, survey)
+score(dag4, data = survey, type="bic")
+
+rnd <- random.graph(nodes = c("A", "S", "E", "O", "R", "T"))
+modelstring(rnd)
+score(rnd, data = survey, type="bic") # so dag is better than random! explains some structure
+
+
