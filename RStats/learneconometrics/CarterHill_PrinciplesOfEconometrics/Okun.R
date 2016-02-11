c349d7a1863572635eff701f83c2a3a53c572304
BabitzdaBrave <amg.vintila@gmail.com>
2016-02-11 07:56:33 -0800

    mull over more

--- RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Okun.R
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Okun.R
@@ -4,6 +4,7 @@ www <- "http://www.econometrics.com/comdata/hill4/okun.dat"
 okun <- read.table(www, header=TRUE)
 head(okun)
 
+# Method 1 of creating the time vector (hard way)
 # make col1 - time
 years <- seq(1985, 2009)
 years <- years[2:24]
@@ -19,9 +20,20 @@ DU <- c(NA, diff(okun$U))
 G_1 <- c(NA, okun$G[1:97]); G_1
 G_2 <- c(NA, NA, okun$G[1:96]); G_2
 
-okunData <- data.frame(Quarter=time, Ut=okun$U, Ut_1=U_1, Gt=okun$G, 
+okunData <- data.frame(Quarter=time, Ut=okun$U, DU=DU, Ut_1=U_1, Gt=okun$G, 
                        Gt_1=G_1, Gt_2=G_2)
 head(okunData); tail(okunData)
+
+
+# Method 2 of creating time vector (easy way)
+# uses rolling shift provided by lag() method
+okun.ts <- ts(okunData[,2:ncol(okunData)], start=1985, frequency=4)
+okun.ts <- lag(okun.ts, -1)
+okun.ts
+okun.ts <- na.omit(okun.ts)
+okun.ts
+
+
 
 # Calculate autocorrelation coefficients for Gt
 g <- okunData$Gt 
@@ -36,4 +48,32 @@ c6 <- 1/98 * sum( (g[1:92] - mean(g)) * (g[7:98] - mean(g)) )
 r1 <- c1/c0; r2 <- c2/c0; r3 <- c3/c0; r4 <- c4/c0
 r5 <- c5/c0; r6 <- c6/c0
 r1; r2; r3; r4; r5; r6
+
+# Test significance of autocorrelation coefficients
+# Z = (rk - 0) / sqrt(1/T)
+z1 <- r1*sqrt(98); z1                 # z-statistic
+t1 = r1 / sqrt((1 - r1^2)/(98-2)); t1 # t-statistic
+cor.test(g[1:97], g[2:98])            # so autocorcoef is NOT same as corcoef
+
+# z-statistics
+z2 <- r2*sqrt(98)
+z3 <- r3*sqrt(98)
+z4 <- r4*sqrt(98)
+z5 <- r5*sqrt(98)
+z6 <- r6*sqrt(98)
+# p-values
+p1 <- 1 - pnorm(z1)
+p2 <- 1 - pnorm(z2)
+p3 <- 1 - pnorm(z3)
+p4 <- 1 - pnorm(z4)
+p5 <- 1 - pnorm(z5)
+p6 <- 1 - pnorm(z6)
+tbl <- data.frame(Z=round(c(z1, z2,z3,z4,z5,z6), 5), 
+                  P=round(c(p1,p2,p3,p4,p5,p6), 5))
+tbl
+
+
+
+# Correlogram
+autoplot(acf(okun.ts, lag.max=12, plot=FALSE))
 
