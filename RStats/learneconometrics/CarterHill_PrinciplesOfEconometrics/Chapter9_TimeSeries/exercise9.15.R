db9d143caad33077f0f7c317534b2b54cf0d52be
BabitzdaBrave <amg.vintila@gmail.com>
2016-02-22 23:27:13 -0800

    making lagrange multplier test of autocorrelation of errors after the t-test

--- RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter9_TimeSeries/exercise9.15.R
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter9_TimeSeries/exercise9.15.R
@@ -32,9 +32,10 @@ banglaData <- na.omit(banglaData)
 head(banglaData)
 auxiliary.lm <- lm(data=banglaData, E ~ lnP + E_1)
 summary(auxiliary.lm)
-33*0.1633
 
 
+
+# Lagrange Multiplier Test for Autocorrelation of Errors
 phillips <- read.dta("phillips_aus.dta")
 phillipsData <- data.frame(inf=phillips$inf, du=c(NA, diff(phillips$u)))
 phillipsData <- na.omit(phillipsData)
@@ -48,8 +49,32 @@ phillipsData$E <- e
 phillipsData$E_1 <- c(NA, e[1:89])
 phillipsData <- na.omit(phillipsData)
 head(phillipsData)
+tail(phillipsData)
 
-auxiliary.lm <- lm(data=phillipsData, E ~ du + E_1)
-summary(auxiliary.lm)
+# Lagrange Multiplier Test for Autocorrelation of Errors
+lagrange.lm <- lm(data=phillipsData, E ~ du + E_1)
+summary(lagrange.lm)
 nrow(phillipsData)
 89*0.3102
+
+
+
+# T-test for Autocorrelation of Errors 
+
+## method (i)
+ttest.lm.1 <- lm(data=phillipsData, inf ~ du + E_1)
+summary(ttest.lm.1) # see? t-value is 6.219, df = N-2 = 88-2 = 86
+anova(ttest.lm.1)   # see? F-value is 38.67
+
+## method (ii)
+phillipsData2 <- data.frame(inf=phillips$inf, du=c(NA, diff(phillips$u)))
+phillipsData2 <- na.omit(phillipsData2)
+phillipsData2$E <- e
+phillipsData2$E_1 <- c(0, e[1:89]) # see? put a zero
+head(phillipsData2)
+
+ttest.lm.2 <- lm(data=phillipsData2, inf ~ du + E_1)
+summary(ttest.lm.2)     # see? t-value is 6.202, df = N-2 = 89-2 = 87
+anova(ttest.lm.2)       # see? F-value is 38.47
+
+
