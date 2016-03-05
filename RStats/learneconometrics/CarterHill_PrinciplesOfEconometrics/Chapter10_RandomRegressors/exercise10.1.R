87b67cc17aa6366dcc9ae8ef008793d88824bdac
BabitzdaBrave <amg.vintila@gmail.com>
2016-03-05 10:41:58 -0800

    save

---
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/exercise10.1.R
@@ -0,0 +1,10 @@
+setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors")
+rm(list=ls())
+
+library(lmtest)
+library(foreign)
+library(ggfortify)
+# Data from: http://www.principlesofeconometrics.com/poe4/poe4stata.htm
+
+
+
