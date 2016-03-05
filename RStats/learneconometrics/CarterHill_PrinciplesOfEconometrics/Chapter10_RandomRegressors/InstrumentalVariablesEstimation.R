0426f56d26f9b11fd0f81c3f50818c79ff651ae8
BabitzdaBrave <amg.vintila@gmail.com>
2016-03-04 23:31:31 -0800

    on exercises

--- RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/InstrumentalVariablesEstimation.R
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/InstrumentalVariablesEstimation.R
@@ -12,12 +12,13 @@ mroz <- read.dta("mroz.dta")
 mroz <- mroz[1:428, ]
 
 cor(mroz$mothereduc, mroz$educ) # see? positive correlation -> endogeneity
-original.lm <- lm(data=mroz, lnw ~ educ + exp + exp2)
-
-exp <- mroz$exper; exp2 <- exp^2
-firststage.m.lm <- lm(data=mroz, educ ~ exp + exp2 + mothereduc)
 
 lnw <- log(mroz$wage)
+exp <- mroz$exper; exp2 <- exp^2
+original.lm <- lm(data=mroz, lnw ~ educ + exp + exp2)
+
+firststage.m.lm <- lm(data=mroz, educ ~ exp + exp2 + mothereduc)
+
 educFits <- firststage.m.lm$fitted.values; length(educFits)
 secondstage.m.lm <- lm(lnw ~ educFits + exp + exp2)
 
@@ -38,7 +39,7 @@ firststage.fm.lm <- lm(data=mroz, educ ~ exp + exp2 + mothereduc + fathereduc)
 summary(firststage.fm.lm)
 
 educFits <- firststage.fm.lm$fitted.values
-secondstage.fm.lm <- lm(lnw ~ educFits + exp + exp2)
+secondstage.fm.lm <- lm(lnw ~ educFits + exp + exp2 )
 summary(secondstage.m.lm)
 summary(secondstage.fm.lm)
 
@@ -55,3 +56,34 @@ resEM.lm <- lm(resE ~ resM)
 resEM.lm$coefficients # coef on mothereduc is same as in firststage.m.lm
 # Partial correlation coefficient is defined below: 
 cor(resE, resM) # partial since effects of other variables are netted out. 
+
+
+
+
+## Hausman Test for endogeneity (wage equation)
+summary(firststage.fm.lm)
+vhat <- firststage.fm.lm$residuals
+secondstage.hausman.lm <- lm(data=mroz, lnw ~ educ + exp + exp2 + vhat)
+# NOTE: these estimates are identical to instrumental variable estimates from
+# secondstage.fm.lm
+summary(secondstage.fm.lm)
+summary(secondstage.hausman.lm)
+# p-value from coefficient on vhat is not very significant but still concerning: 
+# so EDUC is somewhat endogeneous (correlated with error)
+
+
+
+
+## Sargan Test for instrumental validity
+summary(secondstage.fm.lm)
+ehat <- secondstage.fm.lm$residuals
+secondstage.sargan.lm <- lm(data=mroz, ehat ~ mothereduc + fathereduc)
+s <- summary(secondstage.sargan.lm); s
+R2 <- s$r.squared; R2
+N <- nrow(mroz); N
+NRsq <- N*R2; NRsq      # TODO: R2 should be 0.000883 - why not the same?
+chi.crit <- qchisq(0.95, df=1); chi.crit
+
+# H0: surplus instrument is valid, H1: it is not, so fail to reject it is valid. 
+# so we are reassured that our instrumental variables estimator for wage equation
+# is consistent. 
