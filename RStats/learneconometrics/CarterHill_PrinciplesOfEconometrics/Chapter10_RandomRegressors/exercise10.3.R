f0eb1a8d0d3c5b2f37bfb79f4a3d6666fac34973
BabitzdaBrave <amg.vintila@gmail.com>
2016-03-09 06:34:21 -0800

    save

--- RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/exercise10.3.R
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/exercise10.3.R
@@ -1,5 +1,10 @@
-setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors")
-source("/datascience/projects/statisticallyfit/github/learningprogramming/R/RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/Formulas.R")
+setwd("/datascience/projects/statisticallyfit/github/
+      learningprogramming/R/RStats/learneconometrics/
+      CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors")
+source("/datascience/projects/statisticallyfit/github/
+       learningprogramming/R/RStats/learneconometrics/
+       CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/
+       Formulas.R")
 rm(list=ls())
 
 library(lmtest)
@@ -13,21 +18,40 @@ brumm <- read.dta("brumm.dta")
 brumm.lm <- lm(data=brumm, inflat ~ money + output)
 summary(brumm.lm)
 
+
+## TODO: help for F-test - how to do it? F.stat must be 10.52
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 # Testing H0: B1 = 0, B2 = 1, B3 = -1
 # Same as H0: B1 = 0, V2 = 0, V3 = 0, where V2=B2-1, V3=B3+1
+q <- 3; n <- 76
 v2 <- brumm$money - 1
 v3 <- brumm$output + 1
 brumm.ftest.lm <- lm(data=brumm, inflat ~ v2 + v3)
 summary(brumm.ftest.lm)
-anova(brumm.ftest.lm)
+sse <- SSE(brumm$inflat, brumm.ftest.lm$fitted.values); sse
+sst <- SST(brumm$inflat); sst
+ssm <- sst - sse; ssm
+dfm <- q; dfm
+dfe <- n - q; dfe 
+MSM <- ssm / dfm; MSM
+MSE <- sse / dfe; MSE
+# This is the joint hypothesis F-test
+F.statistic <- MSM / MSE; F.statistic
+F.crit <- qf(0.95, dfm, dfe); F.crit #ATTENTION! dfm first THEN dfe
+p.value <- 1 - pf(F.statistic, dfm, dfe); p.value
+# This is the single coefficient hypothesis F-test
+anova(brumm.ftest.lm) 
 
 
-## TODO: help for F-test - how to do it?
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
 ## Part b) residuals do they have heteroskedasticity?
-res.df <- data.frame(res=brumm.lm$residuals, fits=brumm.lm$fitted.values)
+res.df <- data.frame(res=brumm.lm$residuals, 
+                     fits=brumm.lm$fitted.values)
 ggplot(data=res.df, aes(x=fits, y=res)) + 
       geom_point(shape=19) + 
       geom_hline(y=0)
@@ -45,7 +69,10 @@ NR2 <- N * R2; NR2
 X.crit <- qchisq(0.95, df=1); X.crit   # reject homoskedasticity
 
 
-## Part c) how do get White HAC stderrors?
+
+
+
+## Part c) how to get White HAC stderrors?
 HACs <- summaryHAC(brumm.lm)
 HACs
 
@@ -56,8 +83,25 @@ HACs
 # 4 instrumentals: INITIAL, SCHOOL, INV, POPRATE
 # 1 endogeneous: OUTPUT
 brumm.lm
-firststage.lm <- lm(data=brumm, output ~ money + initial + school + inv + poprate)
+firststage.lm <- lm(data=brumm, output ~ money + initial + 
+                          school + inv + poprate)
 summary(firststage.lm)
 output.fits <- firststage.lm$fitted.values
 secondstage.lm <- lm(data=brumm, inflat ~ money + output.fits)
 summary(secondstage.lm)
+
+
+
+
+
+## Part f) Hausman test for endogeneity (robust to heteroskedasticity)
+# (but mine is not robust to it)
+
+firststage.lm <- lm(data=brumm, output ~ money + initial + school
+                    + inv + poprate)
+summary(firststage.lm)
+secondstage.lm <- lm(data=brumm, inflat ~ money + output + 
+                           firststage.lm$residuals)
+summary(secondstage.lm)
+
+
