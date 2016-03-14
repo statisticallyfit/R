d593880bb057115949ecc20c687f3712f3440941
BabitzdaBrave <amg.vintila@gmail.com>
2016-03-14 00:43:26 -0700

    on exercise 4 part c)

---
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors/exercise10.4.R
@@ -0,0 +1,13 @@
+setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter10_RandomRegressors")
+rm(list=ls())
+
+library(lmtest)
+library(foreign)
+library(ggfortify)
+# Data from: http://www.principlesofeconometrics.com/poe4/poe4stata.htm
+
+
+## Part a)
+ivreg <- read.dta("ivreg1.dta")
+meanX <- mean(ivreg$x); meanX
+meanE <- mean(ivreg$e); meanE
