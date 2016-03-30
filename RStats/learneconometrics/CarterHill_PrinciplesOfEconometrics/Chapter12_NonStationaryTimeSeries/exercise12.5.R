67967b61c5d7bb15ba25b153da2eae920123b6bc
BabitzdaBrave <amg.vintila@gmail.com>
2016-03-30 11:04:00 -0700

    going to start exercise 12.8 in carter hill

--- RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter12_NonStationaryTimeSeries/exercise12.5.R
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter12_NonStationaryTimeSeries/exercise12.5.R
@@ -28,7 +28,7 @@ db <- c(NA, diff(b))
 lm <- lm(db ~ b_1)
 
 # Looking at below acfs, you need another diffed lag to 
-# get rid of autocorrelation
+# get rid of error autocorrelation
 autoplot(acf(lm$residuals, lag.max=20, plot=F))
 # that means k = 1
 dickeyFullerTest(bond$bond, useTrend=TRUE, k=1) # this is tau
@@ -38,4 +38,24 @@ dickeyFullerTest(bond$bond, useTrend=TRUE, k=1) # this is tau
 
 
 ## Part c) test the first difference for stationarity
-dickeyFullerTest(db, useTrend = TRUE, k = 0)
+autoplot(ts(db)) ## seems fluctuating around constant, no trend
+# tau is less than -2.86 so series is stationary
+dickeyFullerTest(db, useTrend = F, k = 0)
+
+
+
+
+#b <- bond$bond
+#n <- length(b)
+#b_1 <- c(NA, b[1:(n-1)])
+#db <- c(NA, diff(b))
+#ddb <- c(NA, diff(db))
+#db_1 <- c(NA, db[1:101])
+#dat <- data.frame(b=b, b_1=b_1, db=db, db_1=db_1, ddb=ddb)
+#head(dat)
+#summary(lm(ddb ~ db_1))
+
+
+
+
+## Part d) BOND has to be differenced once to get stationary so it is I(1)
---
+++ RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter12_NonStationaryTimeSeries/exercise12.8.R
@@ -0,0 +1,9 @@
+setwd("/datascience/projects/statisticallyfit/github/learningprogramming/R/RStats/learneconometrics/CarterHill_PrinciplesOfEconometrics/Chapter12_NonStationaryTimeSeries")
+rm(list=ls())
+
+library(ggfortify)
+# Data from: http://www.principlesofeconometrics.com/poe4/poe4stata.htm
+
+
+
+## Part a)
